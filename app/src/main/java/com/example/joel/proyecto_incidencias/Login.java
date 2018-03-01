b45d1777556731403f8d4a50e679e1f87ec958cd
Joel Chan <joelchan1220@gmail.com>
2018-02-28 19:38:39 -0600

    cambios purva

--- app/src/main/java/com/example/joel/proyecto_incidencias/Login.java
+++ app/src/main/java/com/example/joel/proyecto_incidencias/Login.java
@@ -10,5 +10,6 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //jhjhjhjhjhjhjhjh
+        //lklkl
     }
 }
