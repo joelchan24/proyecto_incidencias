6cc2d91e2f4ee641ecaf255cece7961ac770accb
Alquen <alquen@outlook.com>
2018-02-28 22:24:54 -0600

    Registro Diseño

--- app/src/main/java/com/example/joel/proyecto_incidencias/Login.java
+++ app/src/main/java/com/example/joel/proyecto_incidencias/Login.java
@@ -1,7 +1,10 @@
 package com.example.joel.proyecto_incidencias;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,8 +12,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //jhjhjhjhjhjhjhjh
-        //lklkl
-        ////
+
+        Button btnRegistro = (Button) findViewById(R.id.btnregistrar);
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, registro.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
