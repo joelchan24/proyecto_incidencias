4e94fc9888af067b1c2dae0fa7d7a4f22ad1e5ec
Adan98 <31905117+Adan98@users.noreply.github.com>
2018-03-13 18:12:57 -0600

    falla al mandar el registro

--- app/src/main/java/com/example/joel/proyecto_incidencias/registro.java
+++ app/src/main/java/com/example/joel/proyecto_incidencias/registro.java
@@ -2,11 +2,15 @@ package com.example.joel.proyecto_incidencias;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -18,16 +22,30 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class registro extends AppCompatActivity implements Response.Listener<JSONObject>,Response.ErrorListener {
     EditText camponombre,campoapellido,campotelefono,campocorreo,campocontra,campofecha,camposexo;
     Button botonagregar;
     ProgressDialog progreso;
     RequestQueue reques;
     JsonObjectRequest jsonObjectRequest;
+    private static final int SELECT_FILE=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Button btngal=(Button)findViewById(R.id.btngaleria);
+        btngal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirgaleria(view);
+            }
+        });
 
         camponombre=(EditText)findViewById(R.id.txtnombreR);
         campoapellido=(EditText)findViewById(R.id.txtapellidoR);
@@ -46,6 +64,34 @@ public class registro extends AppCompatActivity implements Response.Listener<JSO
         });
 
 
+
+    }
+    public  void abrirgaleria(View view){
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Seleccione una imagen"),SELECT_FILE);
+    }
+
+    protected void onActivityResult(int requesCode,int resultCode,Intent data){
+if (resultCode==RESULT_OK){
+
+    Uri selectedImage=data.getData();
+    InputStream is;
+    try {
+        is=getContentResolver().openInputStream(selectedImage);
+        BufferedInputStream bis=new BufferedInputStream(is);
+        Bitmap bitmap= BitmapFactory.decodeStream(bis);
+        ImageView iv=(ImageView)findViewById(R.id.imagenview);
+        iv.setImageBitmap(bitmap);
+
+
+    }catch (FileNotFoundException e){
+        System.out.print("Fallo");
+    }
+}else {
+Toast.makeText(registro.this,"Salio de la galeria",Toast.LENGTH_SHORT).show();
+}
 
     }
     public void cargarwebservice() {
